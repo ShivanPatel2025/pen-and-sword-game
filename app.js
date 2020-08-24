15fe98670f4921e9fd9c720fb551fec228899697
PenAndSwordGame <51036947+PenAndSwordGame@users.noreply.github.com>
2020-08-24 16:54:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname+"./register_test.html");
+  response.sendFile(__dirname+"/register_test.html");
 })
 
 app.listen(app.get('port'), function() {
