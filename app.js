ce378e32dc31107f0759960737b4b29cc610d85c
PenAndSwordGame <51036947+PenAndSwordGame@users.noreply.github.com>
2020-08-24 15:22:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var express = require('express')
 var app = express()
+var sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./pns.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
