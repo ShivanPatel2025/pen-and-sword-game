88b85b8f52442be47e598328cface4aa43dadae1
PenAndSwordGame <51036947+PenAndSwordGame@users.noreply.github.com>
2020-08-24 18:18:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 app.use(express.static("public"));
+
 //SQLITE
 var sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./pns.db', (err) => {
@@ -14,17 +16,24 @@ let db = new sqlite3.Database('./pns.db', (err) => {
 });
 //SQLITE
 
+//RUNNING SERVER
 app.set('port', (process.env.PORT || 5000))
-
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-})
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+//RUNNING SERVER
 
-app.post("/sign-up", (req, res) => {
-  res.send(`username is: ${req.body.user}.`);
+//SENDING LOGIN PAGE
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+})
+//SENDING LOGIN PAGE
+
+
+app.post('/sign-up', urlencodedParser, function (req, res){
+  var reply='';
+  reply += "Your name is" + req.body.user;
+  reply += "Your E-mail id is" + req.body.password; 
+  reply += "Your address is" + req.body.email;
+  res.send(reply);
 });         
