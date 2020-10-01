0b86cb0b1d640c71ec1833a5072010d9849c80f4
PenAndSwordGame <penandswordgame@gmail.com>
2020-09-30 23:33:28 -0700

    ground attack framework

--- routes/warRoute.js
+++ routes/warRoute.js
@@ -59,7 +59,7 @@ router.get('/war', function(req,res) {
                             aggressorstability: rows.aggressorstability,
                             defenderstability: rows.defenderstability,
                             aggressormaps: rows.aggressormaps,
-                            defendermaps: rows.defendermaps
+                            defendermaps: rows.defendermaps 
                         }
                         defensiveWars.push(warObject)
                         //console.log(defensiveWars)
@@ -98,7 +98,7 @@ router.post('/declarewar', urlencodedParser, function(req,res) {
                     if(rows) {
                         console.log('u are already engaged in warfare with this person')
                         res.redirect('/war')
-                    } else if (aggressorid=defenderid){
+                    }else if (storedID==defenderid){
                         console.log('you cant attack urself')
                         res.redirect('/war')
                     }else{
@@ -305,10 +305,93 @@ router.post('/attack', urlencodedParser, function(req,res) {
                 foreignAir = [blimps, harpies, angels, dragons];
                 foreignSea = [galleys, pirates, sea_serpents];
                 foreignSiege = [catapults, trebuchets, cannons];
-                res.render('attack', {domesticAir,domesticGround,domesticSea,domesticSiege,domesticMaps,domesticStability, foreignAir, foreignGround,foreignSea,foreignSiege,foreignMaps,foreignStability});
+                res.render('attack', {domesticAir,domesticGround,domesticSea,domesticSiege,domesticMaps,domesticStability, foreignAir, foreignGround,foreignSea,foreignSiege,foreignMaps,foreignStability, warid:req.body.warid});
         })   
     })
     }}) 
 })
+
+router.post('/groundbattle', urlencodedParser, function(req,res){
+    let storedID;
+    db.get(`SELECT * FROM sessions WHERE cookie=?`, req.session.id, function(err,rows) {
+      if(rows==undefined) {
+        res.redirect ('/')
+        console.log('this bih not signed in')
+      } else{
+        storedID=parseInt(rows.id, 10)
+        let domesticAir=[];
+        let domesticGround=[];
+        let domesticSea=[];
+        let domesticSiege=[];
+        let domesticMaps=req.body.domesticMaps;
+        let domesticStability=req.body.domesticStability;
+        let foreignAir=[];
+        let foreignGround=[];
+        let foreignSea=[];
+        let foreignSiege=[]; 
+        let foreignMaps=req.body.foreignMaps;
+        let foreignStability=req.body.foreignStability;
+        let enemyID;
+        db.serialize(()=>{
+        db.get('SELECT * FROM wars WHERE warid=?', req.body.warid, function(err,rows){
+            if (rows.aggressorid==storedID){
+                enemyID=rows.defenderid;
+            }
+            if (rows.defenderid==storedID) {
+                enemyID=rows.aggressorid
+            }
+            //console.log(enemyID)
+        })
+        db.get('SELECT * FROM military WHERE id=?', storedID, function(err,rows){
+            console.log(enemyID)
+            domesticGround[0]=rows.warriors;
+            domesticGround[1]=rows.archers;
+            domesticGround[2]=rows.cavalry;
+            domesticGround[3]=rows.blacksmiths;
+            domesticGround[4]=rows.priests;
+            domesticGround[5]=rows.mages;
+            //console.log(domesticGround)
+            let attackingPower=Number(domesticGround[0])*4+Number(domesticGround[1])*2+Number(domesticGround[2])*12+Number(domesticGround[3])*3+Number(domesticGround[4])*18+Number(domesticGround[5])*20;
+            db.get('SELECT * FROM military WHERE id=?', enemyID, function(err,rows){
+                console.log(rows);
+                foreignGround[0]=rows.warriors;
+                foreignGround[1]=rows.archers;
+                foreignGround[2]=rows.cavalry;
+                foreignGround[3]=rows.blacksmiths;
+                foreignGround[4]=rows.priests;
+                foreignGround[5]=rows.mages;
+                let defendingPower=Number(foreignGround[0])*.05+Number(foreignGround[1])*6+Number(foreignGround[2])*5+Number(foreignGround[3])*10+Number(foreignGround[4])*15+Number(foreignGround[5])*15;
+                let defenderBonus=2;
+                while(defenderBonus>=1.4) {
+                    defenderBonus= Math.random()+1.001;
+                }
+                console.log(defendingPower)
+                console.log(attackingPower)
+                console.log(defenderBonus)
+                if (attackingPower>1.25*defenderBonus*defendingPower) {
+                    console.log('EXTREME ViCOTRY')
+                } else if (attackingPower>1.1*defenderBonus*defendingPower) {
+                    console.log('Medium VICTORY')
+                } else if (attackingPower>.08*defenderBonus*defendingPower) {
+                    console.log('stalemate')
+                }
+            })
+        })
+    })}
+    })
+})
+
+app.post('/airfight', urlencodedParser, function(req,res){
+
+})
+
+app.post('/navalbattle', urlencodedParser, function(req,res){
+
+})
+
+app.post('/siegeprovince', urlencodedParser, function(req,res){
+
+})
+
 
 module.exports = router; 
