0e9bbc4c17fff259d46d8c466aa561898005041c
PenAndSwordGame <penandswordgame@gmail.com>
2020-11-05 17:13:11 -0700

    trade refactoring

--- routes/tradeRoute.js
+++ routes/tradeRoute.js
@@ -30,7 +30,10 @@ router.get('/trade', function(req,res) {
             console.log('this bih not signed in')
           } else{
       storedID=parseInt(rows.id, 10);
-      let arrayOfObjectTrades =[]; 
+      let arrayOfObjectTrades_global =[]; 
+      let arrayOfObjectTrades_personal =[]; 
+      let arrayOfObjectTrades_my =[]; 
+
       db.serialize(()=>{
           db.each(`SELECT * FROM trades WHERE scope=?`,'global', function(err,rows) {
               let trade = {
@@ -43,12 +46,59 @@ router.get('/trade', function(req,res) {
                   date: rows.date,
                       time: rows.time
               }
-              console.log(trade);
-              arrayOfObjectTrades.push(trade);
+              arrayOfObjectTradesglobal.push(trade);
+              console.log(arrayOfObjectTrades_global)
           })
+          db.get('SELECT * FROM kingdoms WHERE id=?',storedID, function(err,rows) {
+            kingdom=rows.kingdom;
+            db.each(`SELECT * FROM trades WHERE scope=? AND recepient=?`,['personal', kingdom] , function(err,rows) {
+                if(err) {
+                    console.error(err.message);
+                }
+                    let trade = {
+                        id: rows.tradeid,
+                        owner: rows.ownerid,
+                        type: rows.type,
+                        resource: rows.resource,
+                        amount: rows.amount,
+                        ppu: rows.price,
+                        date: rows.date,
+                        time: rows.time
+                    }
+                    console.log(trade);
+                    arrayOfObjectTrades_personal.push(trade)
+                    console.log(arrayOfObjectTrades_personal)
+
+                })
+        })
+        db.get('SELECT * FROM kingdoms WHERE id=?',storedID, function(err,rows) {
+            kingdom=rows.kingdom;
+            console.log(kingdom);
+            db.each(`SELECT * FROM trades WHERE ownerid=?`,[kingdom] , function(err,rows) {
+                if(err) {
+                    console.error(err.message);
+                }
+                console.log(kingdom);
+                console.log(rows);
+                let trade = {
+                    id: rows.tradeid,
+                    owner: rows.ownerid,
+                    type: rows.type,
+                    resource: rows.resource,
+                    amount: rows.amount,
+                    ppu: rows.price,
+                    date: rows.date,
+                    time: rows.time
+                }
+                console.log(trade);
+                arrayOfObjectTrades_my.push(trade);
+                console.log(arrayOfObjectTrades_my)
+
+            })
+        })
           db.get('SELECT * FROM kingdoms WHERE id=?', storedID, function(err,rows) {
-              console.log(arrayOfObjectTrades);
-              res.render('trade', {arrayOfObjectTrades}) 
+              console.log('sending to trade')
+              res.render('trade', {arrayOfObjectTrades_global,arrayOfObjectTrades_personal,arrayOfObjectTrades_my}) 
           })
       })
     }})
@@ -62,7 +112,7 @@ router.post('/filter_trades',urlencodedParser, function(req,res){
             console.log('this bih not signed in')
           } else{
       storedID=parseInt(rows.id, 10);
-      let arrayOfObjectTrades =[]; 
+      let arrayOfObjectTrades_global =[]; 
       db.serialize(()=>{
           let resource = req.body.resource;
           let type=req.body.type;
@@ -82,12 +132,59 @@ router.post('/filter_trades',urlencodedParser, function(req,res){
                       time: rows.time
                 }
                 console.log(trade);
-                arrayOfObjectTrades.push(trade);
+                arrayOfObjectTrades_global.push(trade);
             })
-            db.get('SELECT * FROM kingdoms WHERE id=?', storedID, function(err,rows) {
-                console.log(arrayOfObjectTrades);
-                res.render('trade', {arrayOfObjectTrades}) 
+            db.get('SELECT * FROM kingdoms WHERE id=?',storedID, function(err,rows) {
+                kingdom=rows.kingdom;
+                db.each(`SELECT * FROM trades WHERE scope=? AND recepient=?`,['personal', kingdom] , function(err,rows) {
+                    if(err) {
+                        console.error(err.message);
+                    }
+                        let trade = {
+                            id: rows.tradeid,
+                            owner: rows.ownerid,
+                            type: rows.type,
+                            resource: rows.resource,
+                            amount: rows.amount,
+                            ppu: rows.price,
+                            date: rows.date,
+                            time: rows.time
+                        }
+                        console.log(trade);
+                        arrayOfObjectTrades_personal.push(trade)
+                        console.log(arrayOfObjectTrades_personal)
+    
+                    })
             })
+            db.get('SELECT * FROM kingdoms WHERE id=?',storedID, function(err,rows) {
+                kingdom=rows.kingdom;
+                console.log(kingdom);
+                db.each(`SELECT * FROM trades WHERE ownerid=?`,[kingdom] , function(err,rows) {
+                    if(err) {
+                        console.error(err.message);
+                    }
+                    console.log(kingdom);
+                    console.log(rows);
+                    let trade = {
+                        id: rows.tradeid,
+                        owner: rows.ownerid,
+                        type: rows.type,
+                        resource: rows.resource,
+                        amount: rows.amount,
+                        ppu: rows.price,
+                        date: rows.date,
+                        time: rows.time
+                    }
+                    console.log(trade);
+                    arrayOfObjectTrades_my.push(trade);
+                    console.log(arrayOfObjectTrades_my)
+    
+                })
+            })
+              db.get('SELECT * FROM kingdoms WHERE id=?', storedID, function(err,rows) {
+                  console.log('sending to trade')
+                  res.render('trade', {arrayOfObjectTrades_global,arrayOfObjectTrades_personal,arrayOfObjectTrades_my}) 
+              })
           }
           if (order=='price'){
             db.each(`SELECT * FROM trades ORDER BY ppu ${direction} WHERE scope=? AND type=? AND resource=?`,[scope,type,resource], function(err,rows) {
@@ -102,13 +199,103 @@ router.post('/filter_trades',urlencodedParser, function(req,res){
                       time: rows.time
                 }
                 console.log(trade);
-                arrayOfObjectTrades.push(trade);
+                arrayOfObjectTrades_global.push(trade);
             })
-            db.get('SELECT * FROM kingdoms WHERE id=?', storedID, function(err,rows) {
-                console.log(arrayOfObjectTrades);
-                res.render('trade', {arrayOfObjectTrades}) 
+            db.get('SELECT * FROM kingdoms WHERE id=?',storedID, function(err,rows) {
+                kingdom=rows.kingdom;
+                db.each(`SELECT * FROM trades WHERE scope=? AND recepient=?`,['personal', kingdom] , function(err,rows) {
+                    if(err) {
+                        console.error(err.message);
+                    }
+                        let trade = {
+                            id: rows.tradeid,
+                            owner: rows.ownerid,
+                            type: rows.type,
+                            resource: rows.resource,
+                            amount: rows.amount,
+                            ppu: rows.price,
+                            date: rows.date,
+                            time: rows.time
+                        }
+                        console.log(trade);
+                        arrayOfObjectTrades_personal.push(trade)
+                        console.log(arrayOfObjectTrades_personal)
+    
+                    })
             })
+            db.get('SELECT * FROM kingdoms WHERE id=?',storedID, function(err,rows) {
+                kingdom=rows.kingdom;
+                console.log(kingdom);
+                db.each(`SELECT * FROM trades WHERE ownerid=?`,[kingdom] , function(err,rows) {
+                    if(err) {
+                        console.error(err.message);
+                    }
+                    console.log(kingdom);
+                    console.log(rows);
+                    let trade = {
+                        id: rows.tradeid,
+                        owner: rows.ownerid,
+                        type: rows.type,
+                        resource: rows.resource,
+                        amount: rows.amount,
+                        ppu: rows.price,
+                        date: rows.date,
+                        time: rows.time
+                    }
+                    console.log(trade);
+                    arrayOfObjectTrades_my.push(trade);
+                    console.log(arrayOfObjectTrades_my)
+    
+                })
+            })
+              db.get('SELECT * FROM kingdoms WHERE id=?', storedID, function(err,rows) {
+                  console.log('sending to trade')
+                  res.render('trade', {arrayOfObjectTrades_global,arrayOfObjectTrades_personal,arrayOfObjectTrades_my}) 
+              })
           }
+      })
+    }})
+})
+
+router.get('/personaltrades', function(req,res) {
+    let storedID;
+    db.get(`SELECT * FROM sessions WHERE cookie=?`, req.session.id, function(err,rows) {
+        if(rows==undefined) {
+            res.redirect ('/')
+            console.log('this bih not signed in')
+          } else{
+      storedID=parseInt(rows.id, 10);
+      let arrayOfObjectTrades =[]; 
+      let kingdom;
+      db.serialize(()=>{
+          db.get('SELECT * FROM kingdoms WHERE id=?',storedID, function(err,rows) {
+              kingdom=rows.kingdom;
+              console.log(kingdom);
+              db.each(`SELECT * FROM trades WHERE scope=? AND recepient=?`,['personal', kingdom] , function(err,rows) {
+                  if(err) {
+                      console.error(err.message);
+                  }
+                  console.log(kingdom);
+                  console.log(rows);
+                  let trade = {
+                      id: rows.tradeid,
+                      owner: rows.ownerid,
+                      type: rows.type,
+                      resource: rows.resource,
+                      amount: rows.amount,
+                      ppu: rows.price,
+                      date: rows.date,
+                      time: rows.time
+                  }
+                  console.log(trade);
+                  arrayOfObjectTrades.push(trade);
+                  console.log(arrayOfObjectTrades)
+              })
+          })
+          db.get('SELECT * FROM kingdoms WHERE id=?', storedID, function(err,rows) {
+              console.log(arrayOfObjectTrades);
+              res.render('mytrades', {arrayOfObjectTrades}) 
+          })
       })
     }})
 })
@@ -127,7 +314,7 @@ router.get('/mytrades', function(req,res) {
           db.get('SELECT * FROM kingdoms WHERE id=?',storedID, function(err,rows) {
               kingdom=rows.kingdom;
               console.log(kingdom);
-              db.each(`SELECT * FROM trades WHERE scope=? AND recepient=?`,['personal', kingdom] , function(err,rows) {
+              db.each(`SELECT * FROM trades WHERE ownerid=?`,[kingdom] , function(err,rows) {
                   if(err) {
                       console.error(err.message);
                   }
