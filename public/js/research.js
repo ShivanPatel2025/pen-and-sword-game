7d4b4bd8175cdeed6e2a2112d43aca43132bafad
projektcorona <admin@projektcorona.com>
2020-11-09 20:30:45 -0700

    research pug

--- public/js/research.js
+++ public/js/research.js
@@ -1,5 +1,7 @@
 const unlocks = document.querySelectorAll('button.discover.unlock')
 const locked_rows = document.querySelectorAll('tr.locked')
+const selectors = document.querySelectorAll('div.research_selector h3')
+const table_containers = document.querySelectorAll('div.research_container div')
 
 unlocks.forEach((u)=>{
     u.addEventListener('mouseover',()=>{
@@ -20,6 +22,35 @@ locked_rows.forEach(r=>{
 
     const blocker = r.lastElementChild;
     blocker.style.height=`${height}px`
+})
 
+selectors.forEach(s=>{
+    s.addEventListener('click',()=>{
+        let id= s.getAttribute('id')
+        let disp= document.querySelector(`div#${id}`)
+        selectors.forEach(sel=>{
+            sel.classList.remove('highlight')
+        })
+        s.classList.add('highlight')
+        table_containers.forEach(t=>{
+            t.classList.remove('top_zero')
+            t.classList.remove('opacity_full')
+            setTimeout(()=>{
+                t.classList.remove('block')
+            },300)
+        })
+        setTimeout(()=>{
+            disp.classList.add('block')
+
+        },305)
+        setTimeout(()=>{
+            disp.classList.add('top_zero')
+            disp.classList.add('opacity_full')
+
+        },400)
+
+
+    })
 
 })
+
